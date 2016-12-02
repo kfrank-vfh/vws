d016e06eaf547b79daac37ca0e8a1056c5c18b36
Kevin Frank <Kevin Frank@192.168.0.11>
2016-12-02 22:01:13 +0100

    PathFinderTest angepasst

--- AStarPathfinding/src/de/kfrank/vfh/vws/pathfinding/dirty/PathFinderTest.java
+++ AStarPathfinding/src/de/kfrank/vfh/vws/pathfinding/dirty/PathFinderTest.java
@@ -5,25 +5,31 @@ import de.kfrank.vfh.vws.pathfinding.dirty.Node;
 public class PathFinderTest {
 
 	public static void main(String[] args) {
-		NodeGrid grid = generateGrid2();
+		NodeGrid grid = generateGrid3();
 		PathFinder.find(grid);
 		sysoutSolution(grid);
 	}
-	
+
 	private static NodeGrid generateGrid1() {
 		NodeGrid grid = new NodeGrid(7, 5, 5, 4, 2, 1);
 		blockNode(grid, 1, 1);
 		blockHorizontalLine(grid, 2, 1, 5);
 		return grid;
 	}
-	
+
 	private static NodeGrid generateGrid2() {
 		NodeGrid grid = new NodeGrid(7, 5, 5, 4, 0, 4);
 		blockVerticalLine(grid, 1, 1, 4);
 		blockHorizontalLine(grid, 2, 1, 5);
 		return grid;
 	}
-	
+
+	private static NodeGrid generateGrid3() {
+		NodeGrid grid = new NodeGrid(7, 5, 5, 4, 0, 4);
+		blockVerticalLine(grid, 1, 0, 4);
+		return grid;
+	}
+
 	private static void blockNode(NodeGrid grid, int x, int y) {
 		Node node = grid.getNode(x, y);
 		if (node != null) {
@@ -42,7 +48,7 @@ public class PathFinderTest {
 			blockNode(grid, x, y);
 		}
 	}
-	
+
 	private static void sysoutSolution(NodeGrid grid) {
 		Node endNode = grid.getEndNode();
 		if (endNode.getParent() == null) {
